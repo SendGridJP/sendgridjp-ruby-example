24068fd9c1e9812c0aba168a8dc58712d5262e4b
awwa500@gmail.com <awwa500@gmail.com>
2014-04-22 17:42:27 +0900

    Add program

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+#gem 'yaml'
+gem 'sendgrid-ruby'
+ 
---
+++ sendgrid-ruby-example.rb
@@ -0,0 +1,30 @@
+require 'sendgrid'
+require 'sendgrid/email'
+require 'yaml'
+
+config = YAML.load_file("config.yml")
+sendgrid_username = config["SENDGRID"]["SENDGRID_USERNAME"]
+sendgrid_password = config["SENDGRID"]["SENDGRID_PASSWORD"]
+from = config["SENDGRID"]["FROM"]
+tos = config["SENDGRID"]["TOS"].split(',')
+
+email = SendGrid::Email.new
+email.set_tos(tos)
+.set_from(from)
+.set_from_name("送信者名")
+.set_subject("[sendgrid-ruby-example] フクロウのお名前はfullnameさん")
+.set_text("familyname さんは何をしていますか？\r\n 彼はplaceにいます。")
+.set_html("<strong> familyname さんは何をしていますか？</strong><br />彼はplaceにいます。")
+.add_substitution("fullname", ["田中 太郎", "佐藤 次郎", "鈴木 三郎"])
+.add_substitution("familyname", ["田中", "佐藤", "鈴木"])
+.add_substitution("place", ["office", "home", "office"])
+.add_section('office', '中野')
+.add_section('home', '目黒')
+.add_category('カテゴリ1')
+.add_header('X-Sent-Using', 'SendGrid-API')
+.add_attachment('gif.gif', 'owl.gif')
+
+sendgrid = SendGrid::SendGrid.new(sendgrid_username, sendgrid_password)
+#sendgrid.debug_output = true # remove comment if you need to see the request
+response = sendgrid.send(email)
+puts response
