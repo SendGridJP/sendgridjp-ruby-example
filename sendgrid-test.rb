e94c712856a2b5e2db5c35f8dda8ac8b969e30e6
awwa500@gmail.com <awwa500@gmail.com>
2014-04-23 10:42:56 +0900

    Add filter test

---
+++ sendgrid-test.rb
@@ -0,0 +1,35 @@
+require 'sendgrid_ruby'
+require 'sendgrid_ruby/version'
+require 'sendgrid_ruby/email'
+require 'yaml'
+
+config = YAML.load_file("config.yml")
+sendgrid_username = config["SENDGRID"]["SENDGRID_USERNAME"]
+sendgrid_password = config["SENDGRID"]["SENDGRID_PASSWORD"]
+from = config["SENDGRID"]["FROM"]
+tos = config["SENDGRID"]["TOS"].split(',')
+
+email = SendgridRuby::Email.new
+email.set_tos(tos)
+.set_from(from)
+.set_from_name("送信者名")
+.set_subject("[sendgrid-ruby-example] フクロウのお名前はfullnameさん")
+.set_text("familyname さんは何をしていますか？\r\n 彼はplaceにいます。")
+.set_html("<strong> familyname さんは何をしていますか？</strong><br />彼はplaceにいます。")
+.add_substitution("fullname", ["田中 太郎", "佐藤 次郎", "鈴木 三郎"])
+.add_substitution("familyname", ["田中", "佐藤", "鈴木"])
+.add_substitution("place", ["office", "home", "office"])
+.add_section('office', '中野')
+.add_section('home', '目黒')
+.add_category('カテゴリ1')
+.add_header('X-Sent-Using', 'SendGrid-API')
+.add_attachment('gif.gif', 'owl.gif')
+.add_filter("gravatar", "enable", 1)
+.add_filter("subscriptiontrack", "enable", 1)
+.add_filter("subscriptiontrack", "text/plain", "配信停止をご希望の場合リンクをクリックしてください: <% %>.")
+.add_filter("subscriptiontrack", "text/html", " 配信停止をご希望の場合<% リンク %>をクリックしてください")
+
+sendgrid = SendgridRuby::Sendgrid.new(sendgrid_username, sendgrid_password)
+sendgrid.debug_output = true # remove comment if you need to see the request
+response = sendgrid.send(email)
+puts response
